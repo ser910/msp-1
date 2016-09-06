6c7a394824d1d926e7fc225e2a8d749c0f15b345
ivoshkulat <igor.voshkulat@gmail.com>
2016-09-06 21:41:18 +0300

    Max (#13)

---
+++ max.cs
@@ -0,0 +1,16 @@
+class Max : IOperation
+    {
+        public double result
+        {
+            get
+            {
+                return 0;
+            }
+            set {  }
+        }
+        public double variable_1 { get {return 0}; set; }
+        public double variable_2 { get {return 0}; set; }
+
+        public int opp_id =>return 0;
+        public float Status => return 0;
+    }
