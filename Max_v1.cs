b543e821815f9f75429f91840432a104b0749ec5
ninaKh <nina.khanu@gmail.com>
2016-09-06 21:52:44 +0300

    v1 (#20)

---
+++ Max_v1.cs
@@ -0,0 +1,36 @@
+class Max:IOperation
+    {
+       
+
+        
+
+        public double result
+        {
+            get
+            {
+                
+                return 0;
+            }
+            set
+            {
+                ;
+            }
+        }
+        public double variable_1 { get return 0; set; }
+        public double variable_2 { get return 0; set; }
+        public int opp_id
+        {
+            get { return 0; }
+            set { ; }
+        }
+
+        public float status
+        {
+            get { return 0; }
+        }
+
+
+
+        
+    }
+  }
