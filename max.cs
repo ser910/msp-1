4a297e4c7474fbe63912bdb4f25f566aae95d582
ser910 <degtiar.sergey@gmail.com>
2016-09-06 21:40:28 +0300

    Create max.cs
    add new max

---
+++ max.cs
@@ -0,0 +1,13 @@
+class Max : IOperation
+{
+    public double result {get return 0;set;}
+    public double variable_1 { get; set; }
+    public double variable_2 { get; set; }
+        
+        public int opp_id 
+        {
+            get { return my_opp_id; }
+            set { throw new Exception(); }
+        }
+public float status{ get{ return 0;}; set; }
+}
